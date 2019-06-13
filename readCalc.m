3d55808f6cd68de349e1fd8105613fe5069751e3
ibaiGorordo <43162939+ibaiGorordo@users.noreply.github.com>
2019-06-13 20:31:49 +0900

    Uploaded the Matlab file to read the .calc file

---
+++ readCalc.m
@@ -0,0 +1,62 @@
+ function [skeleton, timeMat, rightFootContact, leftFootContact] = readCalc(filename)
+
+Header = {'Hips','RightUpLeg','RightLeg','RightFoot','LeftUpLeg','LeftLeg','LeftFoot','RightShoulder','RightArm','RightForeArm','RightHand',...
+'LeftShoulder','LeftArm','LeftForeArm','LeftHand','Head','Neck','Spine3','Spine2','Spine1','Spine','LeftFootContact','RightFootContact',...
+'RightHandThumb1','RightHandThumb2','RightHandThumb3','RightInHandIndex','RightHandIndex1','RightHandIndex2','RightHandIndex3',...
+'RightInHandMiddle','RightHandMiddle1','RightHandMiddle2','RightHandMiddle3','RightInHandRing','RightHandRing1','RightHandRing2',...
+'RightHandRing3','RightInHandPinky','RightHandPinky1','RightHandPinky2','RightHandPinky3','LeftHandThumb1','LeftHandThumb2','LeftHandThumb3',...
+'LeftInHandIndex','LeftHandIndex1','LeftHandIndex2','LeftHandIndex3','LeftInHandMiddle','LeftHandMiddle1','LeftHandMiddle2','LeftHandMiddle3',...
+'LeftInHandRing','LeftHandRing1','LeftHandRing2','LeftHandRing3','LeftInHandPinky','LeftHandPinky1','LeftHandPinky2','LeftHandPinky3'};
+
+
+delimiter = {''};
+formatSpec = '%q%[^\n\r]';
+
+fileID = fopen(filename,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string',  'ReturnOnError', false);
+fclose(fileID);
+textData = [dataArray{1:end-1}];
+
+
+% Read initial direction:
+line = strsplit(textData(1),'\t');
+zDirection = str2double(line(2:end));
+
+% Read the number of bones:
+line = strsplit(textData(2),'\t');
+boneNumber = str2double(line(2));
+
+delimiter = '\t';
+startRow = 7;
+formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+fileID = fopen(filename,'r');
+textscan(fileID, '%[^\n\r]', startRow-1, 'WhiteSpace', '', 'ReturnOnError', false, 'EndOfLine', '\r\n');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string', 'EmptyValue', NaN, 'ReturnOnError', false);
+fclose(fileID);
+dataMat = [dataArray{1:end-1}];
+
+index = 0;
+clear skeleton
+j = 1;
+% timeMat = dataMat(:,1);
+timeMat = 0:1/120:((length(dataMat)-1)/120);
+for i = 1:length(Header)
+    bodyPart = Header{i};
+    if (i ~= 22 && i ~= 23)
+        skeleton(j).Name = bodyPart;
+        skeleton(j).Pos = dataMat(:,index + (1:3));
+        skeleton(j).Vel = dataMat(:,index + (4:6));
+        skeleton(j).quat = dataMat(:,index + (7:10));
+        skeleton(j).Accel = dataMat(:,index + (11:13));
+        skeleton(j).AngVel = dataMat(:,index + (14:16));
+        index = index + 16;
+        j = j + 1;      
+    elseif i == 22
+              leftFootContact = dataMat(:,index + 1);
+            index = index + 1;
+    elseif i == 23
+            rightFootContact = dataMat(:,index + 1);
+            index = index + 1;
+        
+    end
+end
